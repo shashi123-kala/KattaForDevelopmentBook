a80119cced3b5bd41ab888eed54866f005945c44
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-06-21 12:15:10 +0530

    Add: Start Buying a book

--- src/main/java/com/sdb/tdd/CalculateDiscount.java
+++ src/main/java/com/sdb/tdd/CalculateDiscount.java
@@ -1,5 +1,17 @@
 package com.sdb.tdd;
 
+import java.util.List;
+
+import com.sdb.entity.SoftwareDevelopmentBook;
+
 public class CalculateDiscount {
+
+	public double getTotalPrice(List<SoftwareDevelopmentBook> bookList, double dis) {
+		double amtAfterDis = 0.00;
+		int n = bookList.size();
+		double count = n * 50.00;
+		amtAfterDis = count - (count * (dis / 100));
+		return amtAfterDis;
+	}
 
 }
--- src/test/java/com/sdb/tdd/CalculateDiscountTest.java
+++ src/test/java/com/sdb/tdd/CalculateDiscountTest.java
@@ -1,6 +1,9 @@
 package com.sdb.tdd;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class CalculateDiscountTest {
@@ -10,6 +13,15 @@ public class CalculateDiscountTest {
 	public void initializeToBuyBook() {
 		CalculateDiscount calculateDiscount = new CalculateDiscount();
 		assertNotNull(calculateDiscount);
+	}
+
+	@Test
+	public void buyingOneBook() {
+		List<SoftwareDevelopmentBook> sdbList = new ArrayList<SoftwareDevelopmentBook>();
+		double discount = 0;
+		SoftwareDevelopmentBook sdbFirstI = new SoftwareDevelopmentBook("Clean Code (Robert Martin, 2008)");
+		sdbList.add(sdbFirstI);
+		assertEquals(50.0, calculateDiscount.getTotalPrice(sdbList, discount));
 	}
 
 }
